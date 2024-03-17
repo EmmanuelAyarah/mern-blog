5dc3408f89cc5731efffbb09ae39f6e5f78217b3
EmmanuelAyarah <emmanuelayarahh@gmail.com>
2024-03-17 14:03:42 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv, { config } from 'dotenv';
+
+dotenv.config();
+
+
+mongoose.connect(
+    process.env.MONGO
+)
+.then( () =>{
+    console.log('mongodb is connetd');
+}).catch(err =>{
+        console.log(err);
+    });
+
 
 const app = express();
 
