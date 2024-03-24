9283b8a03e2e54f8697f0b0f7f87ea68cae9f34f
EmmanuelAyarah <emmanuelayarahh@gmail.com>
2024-03-24 14:03:10 +0100

    Create SignUp PAGE Ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -13,7 +13,6 @@ export const signup = async (req, res, next) =>{
         email === '' || 
         password === ''){
         next(errorHandler(400, 'All fields are required'));
-
     }
     
     const hashedPassword = bcryptjs.hashSync(password, 10);
